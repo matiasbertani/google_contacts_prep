7857d28198eddd956658afd7825bee2843879816
Matias Bertani <matiasbertani@gmail.com>
2023-12-15 20:53:34 -0300

    chore: backend/bulder: rename attrs using better ones and create a property

--- backend/google_contacts_data_builder.py
+++ backend/google_contacts_data_builder.py
@@ -9,26 +9,26 @@ import pandas as pd
 
 class GoogleContactsDataBuilder:
 
-    def __init__(self, base, col_dni, razon_social, tel_masivo, tel_otros, col_separador, sep_numeros='-') -> None:
+    def __init__(
+        self,
+        uploaded_datesheet: pd.DataFrame,
+        identifier_column: list,
+        full_name_column: list,
+        main_phones_columns: list,
+        other_phones_columns: list,
+        column_to_group_by: list,
+        main_phones_separator: str = '-',
+    ) -> None:
 
-        self.df_datos = base
-        self.PLANILLA_DATOS = base
-        self.DNI = col_dni  # ['Mat. Unica']
-        self.NOMBRE = razon_social  # ['Razon Social']
-        self.MASIVO = tel_masivo  # ['Telefono_1']
-        # ['Telefono_2','Telefono_3','Telefono_4','Telefono_5','Telefono_6','Telefono_7','Telefono_8','Telefono_9']
-        self.OTROS = tel_otros
-        self.SEPARADOR = col_separador  # ['Ejecutivo'
-        # self.OTRO_DISTINTOR =['']
-        self.necesario = self.DNI + self.NOMBRE + self.SEPARADOR + self.MASIVO + self.OTROS
+        self.uploaded_datesheet = uploaded_datesheet
+        self.identifier_column = identifier_column
+        self.full_name_column = full_name_column
+        self.main_phones_column = main_phones_columns
+        self.other_phones_columns = other_phones_columns
+        self.column_to_group_by = column_to_group_by
 
-        self.sep_numeros = sep_numeros
-        # self.TIPO = tipo  # si es M o R segun sea amsivo o otro
-        self.ENCABEZADO_RESULTADO = ["Nombre", "Apellido", "Trabajo"]
-        self.RESULTADO_SUBIDA = pd.DataFrame(columns=self.ENCABEZADO_RESULTADO)
-
-        self.RESULTADOS = dict()
-
+        self.main_phones_separator = main_phones_separator
+        self.results_dict = dict()
         self.results_path = Path() / 'results'
 
     def build_datasheet(self) -> None:
@@ -36,7 +36,9 @@ class GoogleContactsDataBuilder:
         df_contacts = self._get_df_to_work_with()
         df_contacts = self._split_main_phones(df_contacts)
         df_contacts['Ejecutivo'].fillna('sin ejecutivo', inplace=True)
-        df_contacts = df_contacts.melt(id_vars=self.DNI + self.NOMBRE + self.SEPARADOR)
+        df_contacts = df_contacts.melt(
+            id_vars=self.identifier_column + self.full_name_column + self.column_to_group_by
+        )
 
         df_contacts.dropna(axis='index', inplace=True)
 
@@ -46,8 +48,8 @@ class GoogleContactsDataBuilder:
         df_contacts = df_contacts.rename(columns={'Razon Social': 'Apellido', 'value': 'Trabajo'})
         df_contacts = df_contacts.reindex(columns=['Nombre', 'Apellido', 'Trabajo', 'Ejecutivo'])
 
-        for ejecutivo, datos in df_contacts.groupby(df_contacts[self.SEPARADOR[0]]):
-            self.RESULTADOS[ejecutivo] = datos[['Nombre', 'Apellido', 'Trabajo']].copy()
+        for ejecutivo, datos in df_contacts.groupby(df_contacts[self.column_to_group_by[0]]):
+            self.results_dict[ejecutivo] = datos[['Nombre', 'Apellido', 'Trabajo']].copy()
 
         self._save_results()
 
@@ -57,16 +59,26 @@ class GoogleContactsDataBuilder:
         return df
 
     def _get_only_required_columns_from_uploaded_data(self) -> pd.DataFrame:
-        return self.df_datos[self.necesario]
+        return self.uploaded_datesheet[self._required_columns]
+
+    @property
+    def _required_columns(self) -> list:
+        return (
+            self.identifier_column
+            + self.full_name_column
+            + self.column_to_group_by
+            + self.main_phones_column
+            + self.other_phones_columns
+        )
 
     def _rename_df_columns(self, df: pd.DataFrame) -> pd.DataFrame:
-        renaming_dict = {column: f'RIESGO {index}' for index, column in enumerate(self.OTROS, 1)}
-        renaming_dict[self.MASIVO[0]] = 'MASI'
+        renaming_dict = {column: f'RIESGO {index}' for index, column in enumerate(self.other_phones_columns, 1)}
+        renaming_dict[self.main_phones_column[0]] = 'MASI'
         return df.rename(columns=renaming_dict)
 
     def _split_main_phones(self, df: pd.DataFrame) -> pd.DataFrame:
 
-        splitted_main_phones = df['MASI'].str.split('-', expand=True)
+        splitted_main_phones = df['MASI'].str.split(self.main_phones_separator, expand=True)
 
         renaiming_dict = {x: f'MASI {i}' for i, x in enumerate(list(splitted_main_phones.columns), 1)}
         splitted_main_phones = splitted_main_phones.rename(columns=renaiming_dict)
@@ -94,7 +106,7 @@ class GoogleContactsDataBuilder:
 
         try:
             with zipfile.ZipFile('Bases Google.zip', 'w') as csv_zip:
-                for ejec, data in self.RESULTADOS.items():
+                for ejec, data in self.results_dict.items():
                     csv_zip.writestr(
                         self.results_path / f"{ejec}.csv",
                         pd.DataFrame(data).to_csv(sep=';', encoding='ANSI', index=False),
