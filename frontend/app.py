8d974b29c322b29a8f5054f5f3eae0378105efb5
Matias Bertani <matiasbertani@gmail.com>
2024-01-03 21:05:01 -0300

    feat: frontend: use litera bootstrap theme

--- frontend/app.py
+++ frontend/app.py
@@ -4,7 +4,7 @@ import dash_bootstrap_components as dbc
 external = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(
     __name__,
-    external_stylesheets=[dbc.themes.LUMEN]
+    external_stylesheets=[dbc.themes.LITERA]
 )
 app.title = 'Google Contact Preparation'
 server = app.server
